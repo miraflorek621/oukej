45b3269c3008f9b65b74f715981da71a3bc91fd9
Tomáš Bosák <tombos255@gmail.com>
2024-01-18 21:15:20 +0100

    path

--- routes/reservation.js
+++ routes/reservation.js
@@ -3,6 +3,7 @@ require("dotenv/config");
 
 const express = require("express");
 const router = express.Router();
+const path = require("path");
 
 /*
 
--- routes/reservation2.js
+++ routes/reservation2.js
@@ -3,7 +3,7 @@ require("dotenv/config");
 
 const express = require("express");
 const router = express.Router();
-
+const path = require("path");
 
 router.get("/", (req, res) => {
 	return res.sendFile(path.join(__dirname, "..", "frontend", "rezervace_kola.html"));
