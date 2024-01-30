3d62efae2284c49c8bbb1eb43f3a12f34e3158ab
Tomáš Bosák <tombos255@gmail.com>
2024-01-30 21:26:54 +0100

    Rezervace

--- routes/reservation2.js
+++ routes/reservation2.js
@@ -14,6 +14,74 @@ router.get("/", (req, res) => {
 
 router.post("/", async (req, res) => {
 	try {
+		let timeFrom;
+		let timeTo;
+		let result = [];
+
+		for (let i = 0; i < req.body.length; i++) {
+			if (req.body[i].name == "timeFrom") {
+				timeFrom = req.body[i].value;
+			}
+			if (req.body[i].name == "timeTo") {
+				timeTo = req.body[i].value;
+			} else {
+				let temp = await newReservation.findOne({
+					BycicleName: req.body[i].name,
+				});
+				if (temp != null) {
+					result.push(temp);
+				}
+			}
+		}
+
+		if (result.length == 0) {
+			return res
+				.status(400)
+				.json({ message: "Please Select at least one bike" });
+		}
+
+	
+
+		for (let i = 0; i < result.length; i++) {
+			// if there is no reservation for this bike yet then create one
+			if (result[i].ReservationTable.length == 0) {
+				await newReservation.findOneAndUpdate(
+					{ BycicleName: result[i].BycicleName },
+					{
+						$push: {
+							ReservationTable: {
+								timeFrom: timeFrom,
+								timeTo: timeTo,
+							},
+						},
+					}
+				);
+			} else {
+				// if there is a reservation for this bike then check if the new reservation is not in conflict with the old one
+				for (let j = 0; j < result[i].ReservationTable.length; j++) {
+					if (
+						result[i].ReservationTable[j].timeFrom <= timeTo &&
+						result[i].ReservationTable[j].timeTo >= timeFrom
+					) {
+						return res.status(400).json({ message: "Conflict" });
+					}
+					else{
+						await newReservation.findOneAndUpdate(
+							{ BycicleName: result[i].BycicleName },
+							{
+								$push: {
+									ReservationTable: {
+										timeFrom: timeFrom,
+										timeTo: timeTo,
+									},
+								},
+							}
+						);
+					}
+				}
+			}
+		}
+
 		return res.status(200).json({ message: "Success" });
 	} catch (err) {
 		return res.status(500).json({ message: err });
