a811abe08370bc378def9f31ce6eaa4314f6d00d
Tomáš Bosák <tombos255@gmail.com>
2024-01-31 00:18:55 +0100

    Prisel jsem na to

--- routes/kola.js
+++ routes/kola.js
@@ -14,27 +14,38 @@ function isDateInRange(start1, end1, start2, end2) {
 
 router.post("/", async (req, res) => {
 	try {
-
 		const dateFrom = new Date(req.body.timeFrom);
 		const dateTo = new Date(req.body.timeTo);
 
 		let results = await newReservation.find();
 
-		let filteredResults = [];
+		let filteredDateResults = [];
 
 		for (let i = 0; i < results.length; i++) {
 			for (let j = 0; j < results[i].ReservationTable.length; j++) {
-				if(!isDateInRange(new Date(results[i].ReservationTable[j].timeFrom), new Date(results[i].ReservationTable[j].timeTo), dateFrom, dateTo)){
-					filteredResults.push(results[i]);
+				if (
+					!isDateInRange(
+						new Date(results[i].ReservationTable[j].timeFrom),
+						new Date(results[i].ReservationTable[j].timeTo),
+						dateFrom,
+						dateTo
+					)
+				) {
+					filteredDateResults.push(results[i]);
 				}
 			}
-			
-
-			
 		}
 
+		const filtredResults = [];
 
-		return res.json(filteredResults);
+		
+		for (let i = 0; i < filteredDateResults.length; i++) {
+			if(filteredDateResults[i].ReservationTable[0].Quantity < filteredDateResults[i].Quantity){
+				filtredResults.push(filteredDateResults[i]);
+			}
+		}
+
+		return res.json(filtredResults);
 	} catch (error) {
 		return res.status(500).json({ message: error.message });
 	}
