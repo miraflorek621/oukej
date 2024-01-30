85a728fe7a6cd9e0235b7b9c1e0ebf2594899a78
Miroslav <miraflorek621@gmail.com>
2024-01-30 21:28:50 +0100

    asd
    asda

--- frontend/stylesheet/js/description-popup.js
+++ frontend/stylesheet/js/description-popup.js
@@ -1,4 +1,6 @@
 
+//1
+
 document.getElementById("popup-2").style.display = "none";
 let vis = 0;
 
@@ -20,3 +22,50 @@ function description(){
 
 document.getElementById("des-vis").onclick = function() {description()};
 
+
+//2
+
+document.getElementById("popup-3").style.display = "none";
+let vis1 = 0;
+
+function description1(){
+  
+  
+  if (vis1 == 0){
+    document.getElementById("popup-1").style.display = "none";
+    document.getElementById("popup-3").style.display = "block";
+    vis1 = 1;
+  }
+  else{
+    document.getElementById("popup-1").style.display = "block";
+    document.getElementById("popup-3").style.display = "none";
+    vis1 = 0;
+  }
+  
+}
+
+document.getElementById("des-vis1").onclick = function() {description1()};
+
+
+//3
+
+document.getElementById("popup-5").style.display = "none";
+let vis2 = 0;
+
+function description2(){
+  
+  
+  if (vis2 == 0){
+    document.getElementById("popup-4").style.display = "none";
+    document.getElementById("popup-5").style.display = "block";
+    vis2 = 1;
+  }
+  else{
+    document.getElementById("popup-4").style.display = "block";
+    document.getElementById("popup-5").style.display = "none";
+    vis2 = 0;
+  }
+  
+}
+
+document.getElementById("des-vis2").onclick = function() {description2()};
