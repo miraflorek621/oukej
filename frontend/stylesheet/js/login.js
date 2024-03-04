57bd9d93ec6c29c435df51fdebc6a530d89f0578
Miroslav <miraflorek621@gmail.com>
2024-03-04 10:47:11 +0100

    sdf
    sdf

---
+++ frontend/stylesheet/js/login.js
@@ -0,0 +1,3 @@
+function onSubmit() {
+    if (document.getElementById('password').value == '12345678') {window.location.href = 'administrace.html'; }else{ alert('Ujistěte se že jste zadali správné heslo');}
+}
