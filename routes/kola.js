95ddae263c300291e8cdeb96fd789b0515f657c4
Tomáš Bosák <tombos255@gmail.com>
2024-03-12 16:54:40 +0100

    Sus Code

--- routes/kola.js
+++ routes/kola.js
@@ -6,6 +6,7 @@ const router = express.Router();
 const path = require("path");
 const newReservation = require("../models/reservation_model");
 
+
 function isDateInRange(start1, end1, start2, end2) {
 	return (
 		(start1 >= start2 && start1 <= end2) || (end1 >= start2 && end1 <= end2)
@@ -60,6 +61,14 @@ router.post("/", async (req, res) => {
     }
 });
 
+router.post("/vsechny-kola", async (req, res) => {
+    try {
+        const results = await newReservation.find();
+        return res.status(200).json(results);
+    } catch (error) {
+        return res.status(500).json({ message: error.message });
+    }
+});
 
 
 module.exports = router;
