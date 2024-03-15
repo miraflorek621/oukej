565eab0412dadcf34ea553060767f9d8cbdd7942
Miroslav <miraflorek621@gmail.com>
2024-03-15 20:12:14 +0100

    zdfg
    sdfg

--- routes/newBike.js
+++ routes/newBike.js
@@ -6,25 +6,39 @@ const path = require("path");
 const ReservationTable = require("../models/reservation_model");
 
 router.get("/", (req, res) => {
-	return res.render(path.join(__dirname, "..", "frontend", "nove-kolo.html"));
+  return res.render(path.join(__dirname, "..", "frontend", "nove-kolo.html"));
 });
 
 router.post("/", async (req, res) => {
-	// Get the data from the request.body check if they are not null
+  // Get the data from the request.body check if they are not null
 
-	try {
-		console.log(req.body);
+  try {
+    console.log(req.body);
+    /*
 
-		const BycicleName = req.body.BycicleName;
-		const Quantity = req.body.Quantity;
-		const BycicleImage = req.body.BycicleImage;
-		const Brand = req.body.Brand;
+    const { image } = req.files;
+    if (!image) {
+      return res.status(400).json({ status: "No image found" });
+    }
 
-		if (!BycicleName || !Quantity || !BycicleImage || !Brand) {
-			return res.json({ message: "Něco chybí" });
-		}
+    if (!/^image/.test(image.mimetype)){
+		return res.status(400).json({ status: "Wrong file type" });
+	}
 
-		/*
+
+    const BycicleName = req.body.BycicleName;
+    const Quantity = req.body.Quantity;
+    const BycicleImage = req.body.BycicleImage;
+    const Brand = req.body.Brand;
+
+	console.log(req.files)
+	console.log(req.body)
+
+    if (!BycicleName || !Quantity || !BycicleImage || !Brand) {
+      return res.json({ message: "Něco chybí" });
+    }
+
+
     
         const newBike = new ReservationTable({
             BycicleName: BycicleName,
@@ -36,10 +50,12 @@ router.post("/", async (req, res) => {
         await newBike.save();
     
         return res.json({ message: "Kolo bylo úspěšně přidáno" });
-        */
-	} catch (error) {
-		console.log(error.toString());
-	}
+
+
+	*/
+  } catch (error) {
+    console.log(error.toString());
+  }
 });
 
 module.exports = router;
